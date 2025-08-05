432a9222d4e609eca6f9824c755ca6068a66d630
Mohamed68Mohamed68 <demo963210@gmail.com>
2025-08-05 18:03:53 +0300

    create custom note item

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 24,
+        bottom: 24,
+        left: 16,
+      ),
+      decoration: BoxDecoration(
+        color: Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter tips',
+              style: TextStyle(
+                fontSize: 26,
+                color: Colors.black,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(
+                top: 16,
+                bottom: 16,
+              ),
+              child: Text(
+                'Build your career with thrawat samy',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black.withValues(alpha: 0.5),
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 24,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 24,
+            ),
+            child: Text(
+              'May 21,2022',
+              style: TextStyle(
+                color: Colors.black.withValues(alpha: .5),
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
