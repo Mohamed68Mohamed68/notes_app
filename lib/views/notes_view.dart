d296183e5012d6fde437d1d1a0e65c70cf69ff46
Mohamed68Mohamed68 <demo963210@gmail.com>
2025-08-06 17:30:46 +0300

    show modal buttom sheet and add border radius

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,12 +9,21 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return addNoteBottomSheet();
+              });
+        },
+        backgroundColor: Colors.cyanAccent,
         child: Icon(
           Icons.add,
           color: Colors.black,
         ),
-        backgroundColor: Colors.cyanAccent,
       ),
       body: NotesViewBody(),
     );
---
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class addNoteBottomSheet extends StatelessWidget {
+  const addNoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
