7f92fe96c4ac5affd6c5e3e76fef45400f1755e3
Mohamed68Mohamed68 <demo963210@gmail.com>
2025-08-10 20:54:39 +0300

    refresh notes list

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -9,29 +9,26 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => NotesCubit(),
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                context: context,
-                builder: (context) {
-                  return addNoteBottomSheet();
-                });
-          },
-          backgroundColor: Colors.cyanAccent,
-          child: Icon(
-            Icons.add,
-            color: Colors.black,
-          ),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return addNoteBottomSheet();
+              });
+        },
+        backgroundColor: Colors.cyanAccent,
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
         ),
-        body: NotesViewBody(),
       ),
+      body: NotesViewBody(),
     );
   }
 }
--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add%20note%20cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_form.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
@@ -17,6 +18,7 @@ class addNoteBottomSheet extends StatelessWidget {
             print('faileid ${state.errMessage}');
           }
           if (state is AddNoteSuccess) {
+            BlocProvider.of<NotesCubit>(context).fetchAllNote();
             Navigator.pop(context);
           }
         },
